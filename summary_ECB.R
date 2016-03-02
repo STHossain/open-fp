34039595557f679044ce7acf7cd5d939fc23d941
onnokleen <mail@onnokleen.de>
2016-03-02 18:54:38 +0100

    triangle improved

--- summary_ECB.R
+++ summary_ECB.R
@@ -32,10 +32,12 @@ beta.parameters <- function(i) {
   if (length(dist.panel[!is.na(dist.panel)]) == 1) {
     # find l
     dist.panel <- panel[i,15:51] %>% setNames(as.character(seq(-6.5,11.5, by = 0.5)))
-    l <- as.numeric(colnames(dist.panel)[min(which(!is.na(dist.panel[1,])))])
+    dist.panel[dist.panel == 0] <- NA
+    l <- as.numeric(colnames(dist.panel)[min(which(!is.na(dist.panel[1,])))]) - 0.05
     # find r
-    dist.panel <- panel[i,15:51] %>% setNames(seq(-6.1, 11.9, by = 0.5)) 
-    r <- as.numeric(colnames(dist.panel)[max(which(!is.na(dist.panel[1,])))])
+    dist.panel <- panel[i,15:51] %>% setNames(seq(-6.1, 11.9, by = 0.5))
+    dist.panel[dist.panel == 0] <- NA
+    r <- as.numeric(colnames(dist.panel)[max(which(!is.na(dist.panel[1,])))]) + 0.05
     
     
     out = data_frame(panel.id = panel$panel.id[i], issued.period = panel$issued.period[i], target.period = panel$target.period[i],
@@ -51,34 +53,41 @@ beta.parameters <- function(i) {
   if (length(dist.panel[!is.na(dist.panel)]) == 2) {
     # find l
     dist.panel <- panel[i,15:51] %>% setNames(as.character(seq(-6.5,11.5, by = 0.5)))
-    l <- as.numeric(colnames(dist.panel)[min(which(!is.na(dist.panel[1,])))])
+    dist.panel[dist.panel == 0] <- NA
+    l <- as.numeric(colnames(dist.panel)[min(which(!is.na(dist.panel[1,])))]) -0.05
     # find r
-    dist.panel <- panel[i,15:51] %>% setNames(seq(-6.1, 11.9, by = 0.5)) 
-    r <- as.numeric(colnames(dist.panel)[max(which(!is.na(dist.panel[1,])))])
+    dist.panel <- panel[i,15:51] %>% setNames(seq(-6.1, 11.9, by = 0.5))
+    dist.panel[dist.panel == 0] <- NA
+    r <- as.numeric(colnames(dist.panel)[max(which(!is.na(dist.panel[1,])))]) + 0.05
+    
+    prob.left.bin <- as.numeric(dist.panel[!is.na(dist.panel)][1])/100
+    prob.right.bin <- as.numeric(dist.panel[!is.na(dist.panel)][2])/100
     
     # equally weighted bins
-    if(dist.panel[!is.na(dist.panel)][1] == dist.panel[!is.na(dist.panel)][2]) {
+    if(prob.left.bin == prob.right.bin) {
+      l.new <- l
+      r.new <- r
       mean.distr <- triangular.distribution(l, r, (l+r)/2)$mean
       var.distr  <- triangular.distribution(l, r, (l+r)/2)$var
     }
     
     # left bin has less probability mass than right bin
-    if(dist.panel[!is.na(dist.panel)][1] < dist.panel[!is.na(dist.panel)][2]) {
+    if(prob.left.bin < prob.right.bin) {
       
-      l.new <- r - 1/dist.panel[!is.na(dist.panel)][2] %>% as.numeric()
-      r.new <- r
-      c.new <- r - 0.5/dist.panel[!is.na(dist.panel)][2] %>% as.numeric()
+      l.new <- as.numeric(r - 0.5 - 0.5*sqrt(prob.left.bin/2)/(1-sqrt(prob.left.bin/2)) )
+      r.new <- as.numeric(r)
+      c.new <- (r.new + l.new)/2
 
       mean.distr <- triangular.distribution(l.new, r.new, c.new)$mean
       var.distr  <- triangular.distribution(l.new, r.new, c.new)$var
     }
     
     # left bin has more probability mass than right bin
-    if(dist.panel[!is.na(dist.panel)][1] > dist.panel[!is.na(dist.panel)][2]) {
+    if(prob.left.bin > prob.right.bin) {
       
-      l.new <- l
-      c.new <- l + 0.5/dist.panel[!is.na(dist.panel)][1] %>% as.numeric()
-      r.new <- l + 1/dist.panel[!is.na(dist.panel)][1] %>% as.numeric()
+      l.new <- as.numeric(l)
+      r.new <- as.numeric(l + 0.5 + 0.5 * sqrt(prob.right.bin/2)/(1-sqrt(prob.right.bin/2)))
+      c.new <- (r.new + l.new)/2
 
       mean.distr <- triangular.distribution(l.new, r.new, c.new)$mean 
       var.distr  <- triangular.distribution(l.new, r.new, c.new)$var
@@ -88,6 +97,8 @@ beta.parameters <- function(i) {
     out = data_frame(panel.id = panel$panel.id[i], issued.period = panel$issued.period[i], target.period = panel$target.period[i],
                      variable = panel$variable[i],
                      fit.distr = "triangle", l = l, r = r,
+                     l.new = l.new,
+                     r.new = r.new,
                      mean.distr = mean.distr,
                      var.distr  = var.distr)
   }
@@ -99,11 +110,11 @@ beta.parameters <- function(i) {
     # find l
     dist.panel <- panel[i,15:51] %>% setNames(as.character(seq(-6.5,11.5, by = 0.5)))
     dist.panel[dist.panel == 0] <- NA
-    l <- as.numeric(colnames(dist.panel)[min(which(!is.na(dist.panel[1,])))])
+    l <- as.numeric(colnames(dist.panel)[min(which(!is.na(dist.panel[1,])))]) - 0.05
     # find r
     dist.panel <- panel[i,15:51] %>% setNames(seq(-6.1, 11.9, by = 0.5)) 
     dist.panel[dist.panel == 0] <- NA
-    r <- as.numeric(colnames(dist.panel)[max(which(!is.na(dist.panel[1,])))])
+    r <- as.numeric(colnames(dist.panel)[max(which(!is.na(dist.panel[1,])))]) + 0.05
   
     t.grid <- seq(as.numeric(colnames(dist.panel)[min(which(!is.na(dist.panel[1,])))]),
                   as.numeric(colnames(dist.panel)[max(which(!is.na(dist.panel[1,])))]),
@@ -190,7 +201,9 @@ beta.parameters <- function(i) {
                       )
     
   }
-  
+  if (i %% 1000 == 0) {
+    print(i)
+  }
   out
 }
 
@@ -199,11 +212,41 @@ beta.parameters(3)
 
 n <- dim(panel)[1]
 
-n <- 1000
+n <- 50
 
 system.time(
-distribution.panel <- Reduce(full_join, lapply(c(1:n), beta.parameters))
+  distribution.panel <- Reduce(full_join, lapply(c(1:n), beta.parameters))
 )
+
+
+
+
+
+
+distribution.panel.stripped <- distribution.panel %>% select(-fixed.event.or.horizon)
+
+write_rds(distribution.panel.stripped, path = "distribution_panel.rds")
+
+panel.with.beta.distributions <- left_join(panel, distribution.panel.stripped)
+
+write_rds(panel.with.beta.distributions, path = "ecb_panel_with_fitted_distribution.rds")
+write.dta(panel.with.beta.distributions, file = "ecb_panel_with_fitted_distribution.dta")
+
+qplot(mean.distr, point.forecast, data = panel.with.beta.distributions, color = "variable")
+
+ggplot(dat, aes(x=xvar, y=yvar, color=cond)) +
+  geom_point(shape=1) +
+
+plot.data <- panel.with.beta.distributions %>% 
+  #filter(target.year == 2005) %>% 
+  group_by(variable) #%>% 
+  #filter(variable == "Unemployment") %>%
+  #filter(fit.distr == "beta")
+ggplot(plot.data, aes(y=point.forecast, x=mean.distr)) + 
+  geom_point(aes(group = variable, color = variable), size = 1) +
+  xlim(-5,13.5) +
+  ylim(-5,13.5)
+
 
 
 beta.dist.function <- function(t, l, r, a, b) {
